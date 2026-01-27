2cfc6ddfb4fd2f7c68d10d3c08412cb30257a18a
ericmartinezr <eric.martinez@live.cl>
2026-01-27 19:43:55 -0300

    wip

--- includes/utils.js
+++ includes/utils.js
@@ -1,4 +1,4 @@
-function convertDate(column) {
+/*function convertDate(column) {
   const months = {
     Enero: '01',
     Febrero: '02',
@@ -21,4 +21,34 @@ function convertDate(column) {
 
   return `PARSE_DATE('%u %d de %m, %Y', ${formattedCol})`;
 }
+module.exports = { convertDate };
+*/
+
+function convertDate(column) {
+  return `
+    PARSE_DATE('%d-%m-%Y', 
+      REGEXP_REPLACE(
+        REGEXP_REPLACE(${column}, r'^[a-zA-ZáéíóúÁÉÍÓÚ]+ ', ''),
+        r'(\\d+) de ([a-zA-Z]+), (\\d+)', 
+        r'\\1-' || 
+        CASE 
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'enero' THEN '01'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'febrero' THEN '02'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'marzo' THEN '03'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'abril' THEN '04'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'mayo' THEN '05'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'junio' THEN '06'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'julio' THEN '07'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'agosto' THEN '08'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'septiembre' THEN '09'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'octubre' THEN '10'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'noviembre' THEN '11'
+          WHEN LOWER(REGEXP_EXTRACT(${column}, r'de ([a-zA-Z]+),')) = 'diciembre' THEN '12'
+        END
+        || r'-\\3'
+      )
+    )
+  `;
+}
+
 module.exports = { convertDate };
